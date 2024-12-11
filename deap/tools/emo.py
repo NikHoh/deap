0d412e811b6c7f70f925af4def2de0c654b2c7d0
Hohmann, Nikolas <nikolas.hohmann@tu-darmstadt.de>
2024-12-11 15:58:24 +0100

    added tournament selection based on constraint satisfaction degree

--- deap/tools/emo.py
+++ deap/tools/emo.py
@@ -148,7 +148,8 @@ def selTournamentDCD(individuals, k):
     """Tournament selection based on dominance (D) between two individuals, if
     the two individuals do not interdominate the selection is made
     based on crowding distance (CD). The *individuals* sequence length has to
-    be a multiple of 4 only if k is equal to the length of individuals.
+    be a multiple of 4 only if k is equal to the length of individuals. It that is not the case, the function falls
+    back to another version.
     Starting from the beginning of the selected individuals, two consecutive
     individuals will be different (assuming all individuals in the input list
     are unique). Each individual from the input list won't be selected more
@@ -167,7 +168,7 @@ def selTournamentDCD(individuals, k):
         raise ValueError("selTournamentDCD: k must be less than or equal to individuals length")
 
     if k == len(individuals) and k % 4 != 0:
-        raise ValueError("selTournamentDCD: k must be divisible by four if k == len(individuals)")
+        return selTournamentDCDunrestricted(individuals, k)
 
     def tourn(ind1, ind2):
         if ind1.fitness.dominates(ind2.fitness):
@@ -193,6 +194,136 @@ def selTournamentDCD(individuals, k):
         chosen.append(tourn(individuals_1[i + 2], individuals_1[i + 3]))
         chosen.append(tourn(individuals_2[i], individuals_2[i + 1]))
         chosen.append(tourn(individuals_2[i + 2], individuals_2[i + 3]))
+
+    return chosen
+
+def selTournamentDCDunrestricted(individuals, k):
+    """Like selTournamentDCD() but without restrictions in size of k.
+    Tournament selection based on dominance (D) between two individuals, if
+    the two individuals do not interdominate the selection is made
+    based on crowding distance (CD).
+    Starting from the beginning of the selected individuals, two consecutive
+    individuals will be different (assuming all individuals in the input list
+    are unique). Each individual from the input list won't be selected more
+    than twice.
+
+    This selection requires the individuals to have a :attr:`crowding_dist`
+    attribute, which can be set by the :func:`assignCrowdingDist` function.
+
+    :param individuals: A list of individuals to select from.
+    :param k: The number of individuals to select. Must be less than or equal
+              to len(individuals).
+    :returns: A list of selected individuals.
+    """
+
+    def tourn(ind1, ind2):
+        if ind1.fitness.dominates(ind2.fitness):
+            return ind1
+        elif ind2.fitness.dominates(ind1.fitness):
+            return ind2
+
+        if ind1.fitness.crowding_dist < ind2.fitness.crowding_dist:
+            return ind2
+        elif ind1.fitness.crowding_dist > ind2.fitness.crowding_dist:
+            return ind1
+
+        if random.random() <= 0.5:
+            return ind1
+        return ind2
+
+    individuals_1 = random.sample(individuals, len(individuals))
+    individuals_2 = random.sample(individuals, len(individuals))
+
+    chosen = []
+    for _ in range(k):
+        chosen.append(tourn(random.choice(individuals_1),   random.choice(individuals_2)))
+
+    return chosen
+
+
+def selTournamentConstraint(individuals, k):
+    """Tournament selection based on constraint satisfaction. The *individuals* sequence length has to
+    be a multiple of 4 only if k is equal to the length of individuals. It that is not the case, the function falls
+    back to another version.
+    Starting from the beginning of the selected individuals, two consecutive
+    individuals will be different (assuming all individuals in the input list
+    are unique). Each individual from the input list won't be selected more
+    than twice.
+
+    This selection requires the individuals to have a :attr:`constraints`
+    attribute.
+
+    :param individuals: A list of individuals to select from.
+    :param k: The number of individuals to select. Must be less than or equal
+              to len(individuals).
+    :returns: A list of selected individuals.
+    """
+
+    if k > len(individuals):
+        raise ValueError("selTournamentConstraint: k must be less than or equal to individuals length")
+
+    if k == len(individuals) and k % 4 != 0:
+        return selTournamentConstraintunrestricted(individuals, k)
+
+    def tourn(ind1, ind2):
+
+        if sum([cons.value for cons in ind1.constraints]) < sum([cons.value for cons in ind2.constraints]):
+            return ind1
+        elif sum([cons.value for cons in ind1.constraints]) > sum([cons.value for cons in ind2.constraints]):
+            return ind2
+
+        if random.random() <= 0.5:
+            return ind1
+        return ind2
+
+    individuals_1 = random.sample(individuals, len(individuals))
+    individuals_2 = random.sample(individuals, len(individuals))
+
+    chosen = []
+    for i in range(0, k, 4):
+        chosen.append(tourn(individuals_1[i], individuals_1[i + 1]))
+        chosen.append(tourn(individuals_1[i + 2], individuals_1[i + 3]))
+        chosen.append(tourn(individuals_2[i], individuals_2[i + 1]))
+        chosen.append(tourn(individuals_2[i + 2], individuals_2[i + 3]))
+
+    return chosen
+
+def selTournamentConstraintunrestricted(individuals, k):
+    """Like selTournamentConstraint() but without restrictions in size of k.
+    Tournament selection based on dominance (D) between two individuals, if
+    the two individuals do not interdominate the selection is made
+    based on crowding distance (CD).
+    Starting from the beginning of the selected individuals, two consecutive
+    individuals will be different (assuming all individuals in the input list
+    are unique). Each individual from the input list won't be selected more
+    than twice.
+
+    This selection requires the individuals to have a :attr:`crowding_dist`
+    attribute, which can be set by the :func:`assignCrowdingDist` function.
+
+    :param individuals: A list of individuals to select from.
+    :param k: The number of individuals to select. Must be less than or equal
+              to len(individuals).
+    :returns: A list of selected individuals.
+    """
+
+    def tourn(ind1, ind2):
+
+        if sum([cons.value for cons in ind1.constraints]) < sum([cons.value for cons in ind2.constraints]):
+            return ind1
+        elif sum([cons.value for cons in ind1.constraints]) > sum([cons.value for cons in ind2.constraints]):
+            return ind2
+
+        if random.random() <= 0.5:
+            return ind1
+        return ind2
+
+    individuals_1 = random.sample(individuals, len(individuals))
+    individuals_2 = random.sample(individuals, len(individuals))
+
+    chosen = []
+    for _ in range(k):
+        chosen.append(tourn(random.choice(individuals_1),   random.choice(individuals_2)))
 
     return chosen
 
